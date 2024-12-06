bbf007f2d3d3f3299015cd9906dc6ba51c8374ef
npmzero <surajpathak3430@gmail.com>
2024-12-06 20:56:28 +0545

    Properly Formatted code Day Wise

---
+++ NET/labOne/labOne/DayTwoProgram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labOne
+{
+    internal class LabOneProgram
+    {
+     /*   static void Main(string[] args)
+        {
+
+             1st day five questions */
+
+            /*            MyClass obj = new MyClass();
+                        Program program = new Program();
+                        LabStruct StructObject = new LabStruct();
+                        LabStruct EnumObject = new LabStruct();
+                        LabStruct labStruct = new LabStruct();
+
+
+
+                        obj.Hello();
+                        obj.CheckOddEven();
+                        obj.Sum();
+                        obj.ThreeDimension();
+
+
+                        /*  Classes Enums and Structures */
+
+
+            /*            StructObject.MyStructFunction();
+                        EnumObject.EnumFunction();
+
+                        StaticCLass.Display();
+
+                        labStruct.NamedFunction();
+
+                        int x = 10;
+                        Console.WriteLine(x);
+                        labStruct.RefFunction(ref x);
+                        Console.WriteLine(x);
+
+                        int x1, x2, x3;
+                        labStruct.OutFucntion(out x1, out x2, out x3);
+                        Console.WriteLine($"{x1} {x2} {x3}");
+
+                        Console.WriteLine("\n\nPress any key to Exit");
+
+        }
+            */
+    }
+}
--- NET/labOne/labOne/Program.cs
+++ NET/labOne/labOne/Program.cs
@@ -52,44 +52,8 @@ namespace labOne
 
             */
 
-            /* 1st day five questions */
+            
 
-            /*            MyClass obj = new MyClass();
-                        Program program = new Program();
-                        LabStruct StructObject = new LabStruct();
-                        LabStruct EnumObject = new LabStruct();
-                        LabStruct labStruct = new LabStruct();
-
-
-
-                        obj.Hello();
-                        obj.CheckOddEven();
-                        obj.Sum();
-                        obj.ThreeDimension();
-
-
-                        /*  Classes Enums and Structures */
-
-
-            /*            StructObject.MyStructFunction();
-                        EnumObject.EnumFunction();
-
-                        StaticCLass.Display();
-
-                        labStruct.NamedFunction();
-
-                        int x = 10;
-                        Console.WriteLine(x);
-                        labStruct.RefFunction(ref x);
-                        Console.WriteLine(x);
-
-                        int x1, x2, x3;
-                        labStruct.OutFucntion(out x1, out x2, out x3);
-                        Console.WriteLine($"{x1} {x2} {x3}");
-
-                        Console.WriteLine("\n\nPress any key to Exit");
-
-            */
 
 
 
@@ -99,8 +63,6 @@ namespace labOne
             myChild.OverrideMethod();
 
             
-
-
 
             Console.ReadKey();
 
