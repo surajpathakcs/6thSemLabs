ddd77ab614fa513753f29c4fa9a526e4a2ef6dcf
npmzero <surajpathak3430@gmail.com>
2024-12-07 12:14:33 +0545

    Abstract Class and Interface Lab Day4

---
+++ NET/LabFour/DayFour/Abstract.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayFour
+{
+    abstract class abstractTest {
+        public abstract void Show();
+        public abstract void Select();
+        public void Delete()
+        {
+            Console.WriteLine("This is a delete method in Abstract");
+        }
+    }
+
+    interface ITest {
+        void Create();
+        void Update();
+    }
+
+    class Student : abstractTest, ITest {
+
+        public override void Select()
+        {
+            Console.WriteLine("This is Select Method from abstract");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("This is Show Method from asbstract");
+        }
+        public void Create() { 
+            Console.WriteLine("Interface Create Method from Interface"); 
+        }
+        public void Update() { 
+            Console.WriteLine("Interface Update Method from Interface"); 
+        }
+    }
+
+
+
+}
---
+++ NET/LabFour/DayFour/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayFour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Show();
+            student.Select();
+            student.Create();
+            student.Delete();
+            student.Update();
+
+            Console.ReadKey();
+        }
+    }
+}
