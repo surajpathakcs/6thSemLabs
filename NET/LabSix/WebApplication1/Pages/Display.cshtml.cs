b4cbb2d2fb4c5ada0a0462a31f7e4de80ad70d66
npmzero <surajpathak3430@gmail.com>
2025-01-05 10:14:06 +0545

    .net lab 5th Jan

--- NET/LabSix/WebApplication1/Pages/Display.cshtml.cs
+++ NET/LabSix/WebApplication1/Pages/Display.cshtml.cs
@@ -1,12 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace WebApplication1.Pages
 {
+    public class Person { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+    }
     public class DisplayModel : PageModel
     {
+        public List<Person> personList { get; set; } = new List<Person>();
         public void OnGet()
         {
+            string constr = "server=localhost;user=root;password=;database=ncc";
+            MySqlConnection conn = new MySqlConnection(constr);
+            conn.Open();
+
+            string sql = "SELECT * FROM person";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Person person = new Person();
+                person.Id = reader.GetInt32(0);
+                person.Name = reader.GetString(1);
+                person.Email = reader.GetString(2);
+                person.Contact = reader.GetString(3);
+                personList.Add(person);
+;            }
+            conn.Close();
         }
+
+       
     }
 }
